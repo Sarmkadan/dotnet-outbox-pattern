c9bd870b4bfe0b914914b68aea5b53041d33fa38
Vladyslav Zaiets <rutova2@gmail.com>
2021-12-03 08:58:42 +0200

    add GuidGeneratorJson json helpers.

---
+++ Utilities/GuidGeneratorJsonExtensions.cs
@@ -0,0 +1,70 @@
+#nullable enable
+
+// =============================================================================
+// Author: Vladyslav Zaiets | https://sarmkadan.com
+// CTO & Software Architect
+// =============================================================================
+
+using System.Text.Json;
+
+namespace DotnetOutboxPattern.Utilities;
+
+/// <summary>
+/// Provides System.Text.Json serialization extensions for Guid operations
+/// </summary>
+public static class GuidGeneratorJsonExtensions
+{
+    private static readonly JsonSerializerOptions _jsonOptions = new(JsonSerializerDefaults.Web)
+    {
+        WriteIndented = false,
+        PropertyNamingPolicy = JsonNamingPolicy.CamelCase
+    };
+
+    /// <summary>
+    /// Serializes a Guid to JSON string
+    /// </summary>
+    /// <param name="guid">The Guid to serialize</param>
+    /// <param name="indented">Whether to format the JSON with indentation</param>
+    /// <returns>JSON string representation</returns>
+    /// <exception cref="ArgumentNullException">Thrown when guid is not a valid Guid</exception>
+    public static string ToJson(this Guid guid, bool indented = false)
+    {
+        var options = indented
+            ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
+
+        return JsonSerializer.Serialize(guid, options);
+    }
+
+    /// <summary>
+    /// Deserializes a JSON string to a Guid
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <returns>Deserialized Guid</returns>
+    /// <exception cref="JsonException">Thrown when JSON is invalid</exception>
+    public static Guid FromJson(string json)
+    {
+        ArgumentException.ThrowIfNullOrEmpty(json);
+        return JsonSerializer.Deserialize<Guid>(json, _jsonOptions);
+    }
+
+    /// <summary>
+    /// Attempts to deserialize a JSON string to a Guid
+    /// </summary>
+    /// <param name="json">JSON string to deserialize</param>
+    /// <param name="value">Output parameter for the deserialized value</param>
+    /// <returns>True if deserialization succeeded; false otherwise</returns>
+    public static bool TryFromJson(string json, out Guid value)
+    {
+        try
+        {
+            value = FromJson(json);
+            return true;
+        }
+        catch (JsonException)
+        {
+            value = Guid.Empty;
+            return false;
+        }
+    }
+}
