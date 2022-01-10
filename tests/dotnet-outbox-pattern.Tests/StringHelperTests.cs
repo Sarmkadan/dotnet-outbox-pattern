bdb48d374caa5c21b53003410a7da40a32495ebf
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-10 12:07:37 +0200

    Reshuffle StringHelperTests a bit

--- tests/dotnet-outbox-pattern.Tests/StringHelperTests.cs
+++ tests/dotnet-outbox-pattern.Tests/StringHelperTests.cs
@@ -5,12 +5,19 @@ using FluentAssertions;
 
 namespace DotnetOutboxPattern.Tests;
 
+/// <summary>
+/// Tests for the StringHelper class.
+/// </summary>
 public sealed class StringHelperTests
 {
     [Theory]
     [InlineData("hello")]
     public void ComputeSha256Hash_ReturnsExpectedHash(string input)
     {
+        /// <summary>
+        /// Verifies that the ComputeSha256Hash method returns a non-empty hash for a given input string.
+        /// </summary>
+        /// <param name="input">The input string to compute the hash for.</param>
         var hash = StringHelper.ComputeSha256Hash(input);
         hash.Should().NotBeNullOrEmpty();
     }
@@ -22,6 +29,11 @@ public sealed class StringHelperTests
     [InlineData(null, false)]
     public void IsValidEmail_ReturnsExpectedResult(string? email, bool expected)
     {
+        /// <summary>
+        /// Verifies that the IsValidEmail method returns the expected result for a given email address.
+        /// </summary>
+        /// <param name="email">The email address to validate.</param>
+        /// <param name="expected">The expected result of the validation.</param>
         StringHelper.IsValidEmail(email).Should().Be(expected);
     }
 
@@ -31,6 +43,12 @@ public sealed class StringHelperTests
     [InlineData(null, 5, "")]
     public void Truncate_ReturnsExpectedString(string? input, int maxLength, string expected)
     {
+        /// <summary>
+        /// Verifies that the Truncate method returns the expected truncated string for a given input string and maximum length.
+        /// </summary>
+        /// <param name="input">The input string to truncate.</param>
+        /// <param name="maxLength">The maximum length of the output string.</param>
+        /// <param name="expected">The expected truncated string.</param>
         StringHelper.Truncate(input, maxLength).Should().Be(expected);
     }
 
@@ -39,6 +57,11 @@ public sealed class StringHelperTests
     [InlineData("This is a test!", "this-is-a-test")]
     public void ToSlug_ReturnsSlugifiedString(string input, string expected)
     {
+        /// <summary>
+        /// Verifies that the ToSlug method returns the expected slugified string for a given input string.
+        /// </summary>
+        /// <param name="input">The input string to slugify.</param>
+        /// <param name="expected">The expected slugified string.</param>
         StringHelper.ToSlug(input).Should().Be(expected);
     }
 
@@ -47,6 +70,11 @@ public sealed class StringHelperTests
     [InlineData("Already-Kebab-Case", "already-kebab-case")]
     public void ToKebabCase_ReturnsKebabCaseString(string input, string expected)
     {
+        /// <summary>
+        /// Verifies that the ToKebabCase method returns the expected kebab-case string for a given input string.
+        /// </summary>
+        /// <param name="input">The input string to convert to kebab-case.</param>
+        /// <param name="expected">The expected kebab-case string.</param>
         StringHelper.ToKebabCase(input).Should().Be(expected);
     }
 }
