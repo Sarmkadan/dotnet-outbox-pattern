04f12baa5b34bdf87e89289259fc22fbb7d1d40d
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-12 10:16:01 +0200

    tidy up IntegrationTestFixtureJson json handling

--- tests/dotnet-outbox-pattern.Tests/IntegrationTestFixtureJsonExtensions.cs
+++ tests/dotnet-outbox-pattern.Tests/IntegrationTestFixtureJsonExtensions.cs
@@ -15,8 +15,17 @@ public static class IntegrationTestFixtureJsonExtensions
         DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
     };
 
+    /// <summary>
+    /// Serializes an <see cref="IntegrationTestFixture"/> instance to a JSON string.
+    /// </summary>
+    /// <param name="value">The instance to serialize. Cannot be null.</param>
+    /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
+    /// <returns>A JSON string representation of the instance.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
     public static string ToJson(this IntegrationTestFixture value, bool indented = false)
     {
+        ArgumentNullException.ThrowIfNull(value);
+
         var options = indented
             ? new JsonSerializerOptions(_jsonSerializerOptions)
             {
@@ -27,13 +36,32 @@ public static class IntegrationTestFixtureJsonExtensions
         return JsonSerializer.Serialize(value, options);
     }
 
+    /// <summary>
+    /// Deserializes a JSON string to an <see cref="IntegrationTestFixture"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <returns>The deserialized instance, or null if deserialization fails.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
     public static IntegrationTestFixture? FromJson(string json)
     {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
         return JsonSerializer.Deserialize<IntegrationTestFixture>(json, _jsonSerializerOptions);
     }
 
+    /// <summary>
+    /// Attempts to deserialize a JSON string to an <see cref="IntegrationTestFixture"/> instance.
+    /// </summary>
+    /// <param name="json">The JSON string to deserialize. Cannot be null or empty.</param>
+    /// <param name="value">Receives the deserialized instance if successful; otherwise, null.</param>
+    /// <returns>True if deserialization succeeds; otherwise, false.</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="json"/> is null.</exception>
+    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is empty or whitespace.</exception>
     public static bool TryFromJson(string json, out IntegrationTestFixture? value)
     {
+        ArgumentException.ThrowIfNullOrWhiteSpace(json);
+
         try
         {
             value = JsonSerializer.Deserialize<IntegrationTestFixture>(json, _jsonSerializerOptions);
