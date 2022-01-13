4416c7f1684b661f4a2e51c37074390d57f16b5b
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-13 18:48:15 +0200

    More DeadLetter extension methods
    also touched a couple of call sites.

--- tests/dotnet-outbox-pattern.Tests/DeadLetterTestsExtensions.cs
+++ tests/dotnet-outbox-pattern.Tests/DeadLetterTestsExtensions.cs
@@ -3,7 +3,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =====================================================================
+// ===================================================================
 
 using DotnetOutboxPattern.Domain;
 using FluentAssertions;
@@ -21,11 +21,16 @@ public static class DeadLetterTestsExtensions
     /// <param name="errorMessage">Custom error message, defaults to "Test error"</param>
     /// <param name="totalAttempts">Total attempts, defaults to 3</param>
     /// <returns>Configured DeadLetter instance</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="_" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="totalAttempts" /> is negative</exception>
     public static DeadLetter CreateTestDeadLetter(
         this DeadLetterTests _,
         string errorMessage = "Test error",
         int totalAttempts = 3)
     {
+        ArgumentNullException.ThrowIfNull(_);
+        ArgumentOutOfRangeException.ThrowIfLessThan(totalAttempts, 0);
+
         return new DeadLetter
         {
             Id = Guid.NewGuid(),
@@ -57,11 +62,18 @@ public static class DeadLetterTestsExtensions
     /// <param name="failureReason">Failure reason description</param>
     /// <param name="suggestedAction">Suggested resolution action</param>
     /// <returns>Configured DeadLetter instance with failure properties</returns>
+    /// <exception cref="ArgumentNullException"><paramref name="_" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="failureReason" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="suggestedAction" /> is <see langword="null"/></exception>
     public static DeadLetter CreateTestDeadLetterWithFailure(
         this DeadLetterTests _,
         string failureReason,
         string suggestedAction)
     {
+        ArgumentNullException.ThrowIfNull(_);
+        ArgumentNullException.ThrowIfNull(failureReason);
+        ArgumentNullException.ThrowIfNull(suggestedAction);
+
         var deadLetter = CreateTestDeadLetter(_);
         deadLetter.FailureReason = failureReason;
         deadLetter.SuggestedAction = suggestedAction;
@@ -74,12 +86,21 @@ public static class DeadLetterTestsExtensions
     /// <param name="deadLetter">DeadLetter to assert</param>
     /// <param name="expectedErrorMessage">Expected error message</param>
     /// <param name="expectedAttempts">Expected total attempts</param>
+    /// <exception cref="ArgumentNullException"><paramref name="_" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="deadLetter" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="expectedErrorMessage" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentOutOfRangeException"><paramref name="expectedAttempts" /> is negative</exception>
     public static void ShouldHaveErrorState(
         this DeadLetterTests _,
         DeadLetter deadLetter,
         string expectedErrorMessage,
         int expectedAttempts)
     {
+        ArgumentNullException.ThrowIfNull(_);
+        ArgumentNullException.ThrowIfNull(deadLetter);
+        ArgumentNullException.ThrowIfNull(expectedErrorMessage);
+        ArgumentOutOfRangeException.ThrowIfLessThan(expectedAttempts, 0);
+
         deadLetter.ErrorMessage.Should().Be(expectedErrorMessage);
         deadLetter.TotalAttempts.Should().Be(expectedAttempts);
         deadLetter.IsReviewed.Should().BeFalse();
@@ -91,11 +112,18 @@ public static class DeadLetterTestsExtensions
     /// </summary>
     /// <param name="deadLetter">DeadLetter to assert</param>
     /// <param name="expectedReviewNotes">Expected review notes</param>
+    /// <exception cref="ArgumentNullException"><paramref name="_" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="deadLetter" /> is <see langword="null"/></exception>
+    /// <exception cref="ArgumentNullException"><paramref name="expectedReviewNotes" /> is <see langword="null"/></exception>
     public static void ShouldBeReviewed(
         this DeadLetterTests _,
         DeadLetter deadLetter,
         string expectedReviewNotes)
     {
+        ArgumentNullException.ThrowIfNull(_);
+        ArgumentNullException.ThrowIfNull(deadLetter);
+        ArgumentNullException.ThrowIfNull(expectedReviewNotes);
+
         deadLetter.IsReviewed.Should().BeTrue();
         deadLetter.ReviewNotes.Should().Be(expectedReviewNotes);
         deadLetter.ReviewedAt.Should().NotBeNull();
