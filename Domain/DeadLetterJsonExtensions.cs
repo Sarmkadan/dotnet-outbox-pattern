2c575c09ba915bc04d900aa183f5deb517679e06
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-18 13:01:09 +0200

    DeadLetterJson (de)serialization helpers

--- Domain/DeadLetterJsonExtensions.cs
+++ Domain/DeadLetterJsonExtensions.cs
@@ -17,24 +17,17 @@ public static class DeadLetterJsonExtensions
     /// <summary>
     /// Serializes a DeadLetter to JSON string.
     /// </summary>
-    /// <param name="value">The DeadLetter to serialize</param>
-    /// <param name="indented">Whether to format with indentation</param>
-    /// <returns>JSON string representation of the DeadLetter</returns>
+    /// <param name="value">The DeadLetter to serialize.</param>
+    /// <param name="indented">Whether to format with indentation.</param>
+    /// <returns>JSON string representation of the DeadLetter.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="value"/> is <see langword="null"/>.</exception>
     public static string ToJson(this DeadLetter value, bool indented = false)
     {
-        if (value is null)
-        {
-            throw new System.ArgumentNullException(nameof(value));
-        }
+        ArgumentNullException.ThrowIfNull(value);
 
-        var options = _jsonOptions;
-        if (indented)
-        {
-            options = new System.Text.Json.JsonSerializerOptions(_jsonOptions)
-            {
-                WriteIndented = true,
-            };
-        }
+        var options = indented
+            ? new System.Text.Json.JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
+            : _jsonOptions;
 
         return System.Text.Json.JsonSerializer.Serialize(value, options);
     }
@@ -42,9 +35,10 @@ public static class DeadLetterJsonExtensions
     /// <summary>
     /// Deserializes a DeadLetter from JSON string.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <returns>The deserialized DeadLetter, or null if JSON is null or empty</returns>
-    public static DeadLetter? FromJson(string json)
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <returns>The deserialized DeadLetter, or <see langword="null"/> if <paramref name="json"/> is <see langword="null"/>, empty, or whitespace.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static DeadLetter? FromJson(string? json)
     {
         if (string.IsNullOrWhiteSpace(json))
         {
@@ -57,10 +51,11 @@ public static class DeadLetterJsonExtensions
     /// <summary>
     /// Attempts to deserialize a DeadLetter from JSON string.
     /// </summary>
-    /// <param name="json">JSON string to deserialize</param>
-    /// <param name="value">Output parameter for the deserialized DeadLetter</param>
-    /// <returns>True if deserialization succeeded, false otherwise</returns>
-    public static bool TryFromJson(string json, out DeadLetter? value)
+    /// <param name="json">JSON string to deserialize.</param>
+    /// <param name="value">Output parameter for the deserialized DeadLetter.</param>
+    /// <returns><see langword="true"/> if deserialization succeeded; otherwise, <see langword="false"/>.</returns>
+    /// <exception cref="ArgumentNullException">Thrown if <paramref name="json"/> is <see langword="null"/>.</exception>
+    public static bool TryFromJson(string? json, out DeadLetter? value)
     {
         value = null;
 
