e5e9f9dd109e01662b010f0d893c37412d3a8722
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-19 09:36:40 +0200

    fix

--- Configuration/DependencyInjectionExtensions.cs
+++ Configuration/DependencyInjectionExtensions.cs
@@ -2,7 +2,7 @@
 // =============================================================================
 // Author: Vladyslav Zaiets | https://sarmkadan.com
 // CTO & Software Architect
-// =============================================================================
+// =====================================================================
 
 using DotnetOutboxPattern.BackgroundServices;
 using DotnetOutboxPattern.Caching;
@@ -27,6 +27,9 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Adds all Phase 2 features to the service collection
     /// </summary>
+    /// <param name="services">The service collection to configure</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection AddOutboxPatternPhase2(this IServiceCollection services)
     {
         ArgumentNullException.ThrowIfNull(services);
@@ -74,7 +77,7 @@ public static class DependencyInjectionExtensions
 
         // Add performance monitor for metrics
         services.AddSingleton<PerformanceMonitor>();
-        
+
         // Add OpenTelemetry metrics
         services.AddOutboxOpenTelemetry();
         services.AddSingleton<OutboxMetrics>();
@@ -85,8 +88,13 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Configures OpenTelemetry for the outbox pattern.
     /// </summary>
+    /// <param name="services">The service collection to configure</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection AddOutboxOpenTelemetry(this IServiceCollection services)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.AddOpenTelemetry()
             .ConfigureResource(resource => resource.AddService("dotnet-outbox-pattern"))
             .WithMetrics(builder =>
@@ -103,6 +111,10 @@ public static class DependencyInjectionExtensions
     /// Adds middleware for Phase 2 features
     /// Call this in Program.cs after app.Build()
     /// </summary>
+    /// <param name="app">The application builder</param>
+    /// <param name="rateLimitingOptions">Optional rate limiting options</param>
+    /// <returns>The configured application builder</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="app"/> is null</exception>
     public static IApplicationBuilder UseOutboxPatternMiddleware(
         this IApplicationBuilder app,
         RateLimitingOptions? rateLimitingOptions = null)
@@ -121,9 +133,15 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Registers a specific data formatter for export
     /// </summary>
+    /// <typeparam name="T">The formatter type to register</typeparam>
+    /// <param name="services">The service collection</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection AddDataFormatter<T>(this IServiceCollection services)
         where T : class, IDataFormatter
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.AddScoped<IDataFormatter, T>();
         return services;
     }
@@ -131,11 +149,18 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Configures rate limiting options
     /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <param name="requestsPerWindow">Maximum requests per time window</param>
+    /// <param name="windowSeconds">Time window in seconds</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection ConfigureRateLimiting(
         this IServiceCollection services,
         int requestsPerWindow = 1000,
         int windowSeconds = 60)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.Configure<RateLimitingOptions>(options =>
         {
             options.RequestsPerWindow = requestsPerWindow;
@@ -148,12 +173,20 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Configures message archival options
     /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <param name="archiveDaysThreshold">Number of days after which messages are archived</param>
+    /// <param name="archivalIntervalHours">Interval between archival runs in hours</param>
+    /// <param name="batchSize">Number of messages to process in each batch</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection ConfigureMessageArchival(
         this IServiceCollection services,
         int archiveDaysThreshold = 30,
         int archivalIntervalHours = 6,
         int batchSize = 5000)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.Configure<MessageArchivalOptions>(options =>
         {
             options.ArchiveDaysThreshold = archiveDaysThreshold;
@@ -167,12 +200,20 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Configures health check options
     /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <param name="failureRateThreshold">Failure rate threshold for health checks</param>
+    /// <param name="stuckMessageThreshold">Threshold for stuck messages</param>
+    /// <param name="deadLetterThreshold">Threshold for dead letter messages</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
     public static IServiceCollection ConfigureHealthCheck(
         this IServiceCollection services,
         double failureRateThreshold = 0.10,
         int stuckMessageThreshold = 100,
         int deadLetterThreshold = 50)
     {
+        ArgumentNullException.ThrowIfNull(services);
+
         services.Configure<HealthCheckOptions>(options =>
         {
             options.HighFailureRateThreshold = failureRateThreshold;
@@ -186,10 +227,18 @@ public static class DependencyInjectionExtensions
     /// <summary>
     /// Registers HTTP clients for external integrations
     /// </summary>
+    /// <param name="services">The service collection</param>
+    /// <param name="clients">Collection of named HTTP client configurations</param>
+    /// <returns>The configured service collection</returns>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="services"/> is null</exception>
+    /// <exception cref="ArgumentNullException">Thrown when <paramref name="clients"/> is null</exception>
     public static IServiceCollection AddExternalHttpClients(
         this IServiceCollection services,
         params (string Name, HttpClientConfig Config)[] clients)
     {
+        ArgumentNullException.ThrowIfNull(services);
+        ArgumentNullException.ThrowIfNull(clients);
+
         var factory = new CustomHttpClientFactory(
             new LoggerFactory().CreateLogger<CustomHttpClientFactory>());
 
