9b56a91c7fe6ad9e14efd627a2afdc6c8220bba4
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-19 08:14:06 +0200

    Add OutboxConfigurationBuilderJson json helpers

--- Configuration/OutboxConfigurationBuilderJsonExtensions.cs
+++ Configuration/OutboxConfigurationBuilderJsonExtensions.cs
@@ -3,60 +3,84 @@ using System.Text.Json;
 
 namespace DotnetOutboxPattern.Configuration
 {
-	/// <summary>
-	/// JSON (de)serialization helpers for <see cref="OutboxConfigurationBuilder"/>.
-	/// </summary>
-	public static class OutboxConfigurationBuilderJsonExtensions
-	{
-		// Cached options: camelCase naming, no indentation by default.
-		private static readonly JsonSerializerOptions _options = new()
-		{
-			PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
-			WriteIndented = false
-		};
+    /// <summary>
+    /// JSON (de)serialization helpers for <see cref="OutboxConfigurationBuilder"/>.
+    /// </summary>
+    public static class OutboxConfigurationBuilderJsonExtensions
+    {
+        // Cached options: camelCase naming, no indentation by default.
+        private static readonly JsonSerializerOptions _options = new()
+        {
+            PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            WriteIndented = false
+        };
 
-		/// <summary>
-		/// Serializes the <see cref="OutboxConfigurationBuilder"/> instance to JSON.
-		/// </summary>
-		/// <param name="value">The builder instance to serialize.</param>
-		/// <param name="indented">If true, the output JSON will be indented.</param>
-		/// <returns>A JSON string representing the builder.</returns>
-		/// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
-		public static string ToJson(this OutboxConfigurationBuilder value, bool indented = false)
-		{
-			ArgumentNullException.ThrowIfNull(value);
+        /// <summary>
+        /// Serializes the <see cref="OutboxConfigurationBuilder"/> instance to JSON.
+        /// </summary>
+        /// <param name="value">The builder instance to serialize.</param>
+        /// <param name="indented">If true, the output JSON will be indented.</param>
+        /// <returns>A JSON string representing the builder.</returns>
+        /// <exception cref="ArgumentNullException"><paramref name="value"/> is <see langword="null"/>.</exception>
+        public static string ToJson(this OutboxConfigurationBuilder value, bool indented = false)
+        {
+            ArgumentNullException.ThrowIfNull(value);
 
-			// Use the cached options, optionally overriding indentation.
-			var options = indented
-				? new JsonSerializerOptions(_options) { WriteIndented = true }
-				: _options;
+            // Use the cached options, optionally overriding indentation.
+            var options = indented
+                ? new JsonSerializerOptions(_options) { WriteIndented = true }
+                : _options;
 
-			return JsonSerializer.Serialize(value, options);
-		}
+            return JsonSerializer.Serialize(value, options);
+        }
 
-		/// <summary>
-		/// Deserializes a JSON string into an <see cref="OutboxConfigurationBuilder"/> instance.
-		/// </summary>
-		/// <param name="json">The JSON representation of the builder.</param>
-		/// <returns>The deserialized builder, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
-		/// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized into a <see cref="OutboxConfigurationBuilder"/>.</exception>
-		public static OutboxConfigurationBuilder? FromJson(string json)
-		{
-			if (string.IsNullOrWhiteSpace(json))
-			{
-				return null;
-			}
+        /// <summary>
+        /// Deserializes a JSON string into an <see cref="OutboxConfigurationBuilder"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON representation of the builder.</param>
+        /// <returns>The deserialized builder, or <see langword="null"/> if the JSON is empty or whitespace.</returns>
+        /// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized into a <see cref="OutboxConfigurationBuilder"/>.</exception>
+        /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
+        public static OutboxConfigurationBuilder? FromJson(string json)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(json);
 
-			return JsonSerializer.Deserialize<OutboxConfigurationBuilder>(json, _options);
-		}
+            if (string.IsNullOrWhiteSpace(json))
+            {
+                return null;
+            }
 
-		/// <summary>
-		/// Attempts to deserialize a JSON string into an <see cref="OutboxConfigurationBuilder"/> instance.
-		/// </summary>
-		/// <param name="json">The JSON representation of the builder.</param>
-		/// <param name="value">When the method returns, contains the deserialized builder if successful; otherwise <see langword="null"/>.</param>
-		/// <returns><see langword="true"/> if deserialization succeeded; <see langword="false"/> if a <see cref="JsonException"/> was thrown.</returns>
-		public static bool TryFromJson(string json, out OutboxConfigurationBuilder? value)
-			=> (value = FromJson(json)) is not null;
-	}
+            try
+            {
+                return JsonSerializer.Deserialize<OutboxConfigurationBuilder>(json, _options);
+            }
+            catch (JsonException ex)
+            {
+                throw new JsonException($"Failed to deserialize JSON: {ex.Message}", ex);
+            }
+        }
+
+        /// <summary>
+        /// Attempts to deserialize a JSON string into an <see cref="OutboxConfigurationBuilder"/> instance.
+        /// </summary>
+        /// <param name="json">The JSON representation of the builder.</param>
+        /// <param name="value">When the method returns, contains the deserialized builder if successful; otherwise <see langword="null"/>.</param>
+        /// <returns><see langword="true"/> if deserialization succeeded; <see langword="false"/> if a <see cref="JsonException"/> was thrown.</returns>
+        /// <exception cref="ArgumentException"><paramref name="json"/> is <see langword="null"/> or empty.</exception>
+        public static bool TryFromJson(string json, out OutboxConfigurationBuilder? value)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(json);
+
+            try
+            {
+                value = FromJson(json);
+                return value is not null;
+            }
+            catch (JsonException)
+            {
+                value = null;
+                return false;
+            }
+        }
+    }
 }
