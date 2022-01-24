4375ad43272183bfe9e7bfed5d30a270e2617167
Vladyslav Zaiets <rutova2@gmail.com>
2022-01-24 10:08:07 +0200

    dedupe some logic in MessagePublishingServiceTests
    should be backwards compatible.

--- tests/dotnet-outbox-pattern.Tests/MessagePublishingServiceTests.cs
+++ tests/dotnet-outbox-pattern.Tests/MessagePublishingServiceTests.cs
@@ -10,6 +10,11 @@ using Moq;
 
 namespace DotnetOutboxPattern.Tests;
 
+/// <summary>
+/// Contains unit tests for the <see cref="MessagePublishingService"/> class.
+/// Tests various scenarios including message processing, error handling, locking, scheduling,
+/// and dead-letter queue operations to ensure the outbox pattern message publishing works correctly.
+/// </summary>
 public sealed class MessagePublishingServiceTests
 {
     private readonly Mock<IOutboxRepository> _outboxRepoMock;
@@ -19,6 +24,10 @@ public sealed class MessagePublishingServiceTests
     private readonly PublishingOptions _options;
     private readonly MessagePublishingService _sut;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MessagePublishingServiceTests"/> class.
+    /// Sets up mock dependencies and creates a test instance of <see cref="MessagePublishingService"/> with default options.
+    /// </summary>
     public MessagePublishingServiceTests()
     {
         _outboxRepoMock = new Mock<IOutboxRepository>();
@@ -34,6 +43,9 @@ public sealed class MessagePublishingServiceTests
             _options);
     }
 
+    /// <summary>
+    /// Tests that the constructor throws <see cref="ArgumentNullException"/> when the outbox repository is null.
+    /// </summary>
     [Fact]
     public void Constructor_WithNullOutboxRepository_ThrowsArgumentNullException()
     {
@@ -47,6 +59,9 @@ public sealed class MessagePublishingServiceTests
         act.Should().Throw<ArgumentNullException>().WithParameterName("outboxRepository");
     }
 
+    /// <summary>
+    /// Tests that the constructor throws <see cref="ArgumentNullException"/> when the message publisher is null.
+    /// </summary>
     [Fact]
     public void Constructor_WithNullPublisher_ThrowsArgumentNullException()
     {
@@ -60,6 +75,10 @@ public sealed class MessagePublishingServiceTests
         act.Should().Throw<ArgumentNullException>().WithParameterName("publisher");
     }
 
+    /// <summary>
+    /// Tests that processing an empty batch of messages returns zero processed and zero failed counts.
+    /// Verifies that no publishing attempts are made when there are no pending messages.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WithEmptyBatch_ReturnsZeroProcessed()
     {
@@ -74,6 +93,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that processing a single pending message publishes it and marks it as published.
+    /// Verifies that the message state is updated to <see cref="OutboxMessageState.Published"/> after successful publishing.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WithSingleMessage_PublishesAndMarksPublished()
     {
@@ -95,6 +118,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(message, It.IsAny<CancellationToken>()), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that when the publisher throws an exception, the failure is recorded and processing continues with remaining messages.
+    /// Verifies that the service handles exceptions gracefully and updates the failure count appropriately.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WhenPublisherThrows_RecordsFailureAndContinues()
     {
@@ -119,6 +146,10 @@ public sealed class MessagePublishingServiceTests
         result.FailedCount.Should().Be(1);
     }
 
+    /// <summary>
+    /// Tests that processing a locked message returns false and does not attempt to publish.
+    /// Verifies that messages with <see cref="OutboxMessage.IsLocked"/> set to true are skipped.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenMessageLocked_ReturnsFalse()
     {
@@ -136,6 +167,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that a message that can be retried attempts publishing when processed.
+    /// Verifies that a message with no lock and no scheduling constraint is published successfully.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenMessageCanRetry_AttemptsPublish()
     {
@@ -156,6 +191,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(message, It.IsAny<CancellationToken>()), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that when maximum retry attempts are reached, the message is moved to the dead-letter queue.
+    /// Verifies that messages with publish attempts equal to max attempts are handled appropriately.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenReachedMaxRetries_MovesToDlq()
     {
@@ -183,6 +222,10 @@ public sealed class MessagePublishingServiceTests
         _dlRepoMock.Verify(r => r.AddAsync(It.IsAny<DeadLetter>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that messages scheduled for the future are not processed.
+    /// Verifies that messages with <see cref="OutboxMessage.ScheduledFor"/> in the future are skipped during processing.
+    /// </summary>
     [Fact]
     public async Task ProcessScheduledMessagesAsync_WithFutureSchedule_DoesNotProcess()
     {
@@ -198,6 +241,10 @@ public sealed class MessagePublishingServiceTests
         result.ProcessedCount.Should().Be(0);
     }
 
+    /// <summary>
+    /// Tests that the lock is released for messages with expired locks.
+    /// Verifies that messages with <see cref="OutboxMessage.LockExpiresAt"/> in the past have their <see cref="OutboxMessage.IsLocked"/> set to false.
+    /// </summary>
     [Fact]
     public async Task ReleaseLockAsync_UnlocksExpiredMessage()
     {
@@ -218,6 +265,10 @@ public sealed class MessagePublishingServiceTests
         _outboxRepoMock.Verify(r => r.UpdateAsync(message, It.IsAny<CancellationToken>()), Times.Once);
     }
 
+    /// <summary>
+    /// Tests that locked messages are not processed during batch processing.
+    /// Verifies that messages with <see cref="OutboxMessage.IsLocked"/> set to true are skipped when processing pending messages.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WhenMessageIsLocked_DoesNotProcess()
     {
@@ -236,6 +287,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that scheduled messages are not processed during batch processing.
+    /// Verifies that messages with <see cref="OutboxMessage.ScheduledFor"/> set to a future date are skipped when processing pending messages.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WhenMessageIsScheduled_DoesNotProcess()
     {
@@ -252,6 +307,10 @@ public sealed class MessagePublishingServiceTests
         result.FailedCount.Should().Be(0);
     }
 
+    /// <summary>
+    /// Tests that when the publisher throws exceptions for multiple messages, all failures are recorded.
+    /// Verifies that the service continues processing even when multiple messages fail to publish.
+    /// </summary>
     [Fact]
     public async Task ProcessPendingMessagesAsync_WhenPublisherThrowsForAllMessages_RecordsAllFailures()
     {
@@ -277,6 +336,10 @@ public sealed class MessagePublishingServiceTests
         result.FailedCount.Should().Be(2);
     }
 
+    /// <summary>
+    /// Tests that processing a null message returns false without throwing an exception.
+    /// Verifies that the service handles non-existent messages gracefully.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenMessageIsNull_ReturnsFalse()
     {
@@ -291,6 +354,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that processing a locked message returns false during single message processing.
+    /// Verifies that messages with <see cref="OutboxMessage.IsLocked"/> set to true are skipped when processing individual messages.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenMessageIsLocked_ReturnsFalse()
     {
@@ -308,6 +375,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that processing a scheduled message returns false during single message processing.
+    /// Verifies that messages with <see cref="OutboxMessage.ScheduledFor"/> set to a future date are skipped when processing individual messages.
+    /// </summary>
     [Fact]
     public async Task ProcessSingleMessageAsync_WhenMessageIsScheduled_ReturnsFalse()
     {
@@ -324,6 +395,10 @@ public sealed class MessagePublishingServiceTests
         _publisherMock.Verify(p => p.PublishAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that messages scheduled in the past are processed during scheduled message processing.
+    /// Verifies that messages with <see cref="OutboxMessage.ScheduledFor"/> in the past are published successfully.
+    /// </summary>
     [Fact]
     public async Task ProcessScheduledMessagesAsync_WithPastSchedule_ProcessesMessages()
     {
@@ -346,6 +421,10 @@ public sealed class MessagePublishingServiceTests
         message.State.Should().Be(OutboxMessageState.Published);
     }
 
+    /// <summary>
+    /// Tests that releasing a lock for a non-existent message does not throw an exception.
+    /// Verifies that the service handles missing messages gracefully during lock release operations.
+    /// </summary>
     [Fact]
     public async Task ReleaseLockAsync_WhenMessageNotFound_DoesNotThrow()
     {
@@ -359,6 +438,10 @@ public sealed class MessagePublishingServiceTests
         _outboxRepoMock.Verify(r => r.UpdateAsync(It.IsAny<OutboxMessage>(), It.IsAny<CancellationToken>()), Times.Never);
     }
 
+    /// <summary>
+    /// Tests that releasing a lock for a message that is not locked does not update the message.
+    /// Verifies that the service only updates messages that are actually locked.
+    /// </summary>
     [Fact]
     public async Task ReleaseLockAsync_WhenMessageNotLocked_DoesNotUpdate()
     {
