dde5a53a17227d72e164f82cbcd20cdeb2f1bff6
Vladyslav Zaiets <rutova2@gmail.com>
2026-07-17 21:30:00 +0300

    Tidy up OutboxMessageControllerJsonExtensions

--- Controllers/OutboxMessageControllerJsonExtensions.cs
+++ Controllers/OutboxMessageControllerJsonExtensions.cs
@@ -12,6 +12,8 @@ namespace DotnetOutboxPattern.Controllers;
 
 /// <summary>
 /// Provides System.Text.Json serialization and deserialization extensions for <see cref="OutboxMessageController"/>.
+/// This static class contains utility methods for converting <see cref="OutboxMessageController"/> instances
+/// to and from JSON format using consistent serialization settings.
 /// </summary>
 public static class OutboxMessageControllerJsonExtensions
 {
@@ -30,24 +32,19 @@ public static class OutboxMessageControllerJsonExtensions
     /// <param name="value">The controller instance to serialize.</param>
     /// <param name="indented">Whether to format the JSON with indentation for readability.</param>
     /// <returns>A JSON string representation of the controller.</returns>
-    /// <exception cref="ArgumentNullException">Thrown when <paramref name="value"/> is null.</exception>
-    public static string ToJson(this OutboxMessageController value, bool indented = false)
-    {
-        ArgumentNullException.ThrowIfNull(value);
-
-        var options = indented
+    /// <exception cref="ArgumentNullException"><paramref name="value"/> is null.</exception>
+    public static string ToJson(this OutboxMessageController value, bool indented = false) =>
+        JsonSerializer.Serialize(value, indented
             ? new JsonSerializerOptions(_jsonOptions) { WriteIndented = true }
-            : _jsonOptions;
-
-        return JsonSerializer.Serialize(value, options);
-    }
+            : _jsonOptions);
 
     /// <summary>
     /// Deserializes a JSON string to an <see cref="OutboxMessageController"/> instance.
     /// </summary>
     /// <param name="json">The JSON string to deserialize.</param>
     /// <returns>The deserialized controller instance, or null if deserialization fails.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="JsonException">Thrown when the JSON is malformed or cannot be deserialized to <see cref="OutboxMessageController"/>.</exception>
     public static OutboxMessageController? FromJson(string json)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
@@ -68,7 +65,7 @@ public static class OutboxMessageControllerJsonExtensions
     /// <param name="json">The JSON string to deserialize.</param>
     /// <param name="value">Receives the deserialized controller instance if successful.</param>
     /// <returns>True if deserialization succeeds; otherwise, false.</returns>
-    /// <exception cref="ArgumentException">Thrown when <paramref name="json"/> is null or empty.</exception>
+    /// <exception cref="ArgumentException"><paramref name="json"/> is null or empty.</exception>
     public static bool TryFromJson(string json, out OutboxMessageController? value)
     {
         ArgumentException.ThrowIfNullOrEmpty(json);
