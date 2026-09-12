0223a62eccee1e75fa5219b15502f60360e4c57d
Vladyslav Zaiets <rutova2@gmail.com>
2026-09-12 08:52:44 +0000

    chore: apply codex edits

--- Exceptions/HttpRequestException.cs
+++ Exceptions/HttpRequestException.cs
@@ -3,25 +3,33 @@
 namespace DotnetOutboxPattern.Exceptions;
 
 /// <summary>
-/// Exception thrown when HTTP requests fail
+/// Represents an error that occurs when an HTTP request fails.
 /// </summary>
 public sealed class HttpRequestException : OutboxException
 {
     /// <summary>
-    /// HTTP status code if available
+    /// Gets the HTTP status code returned by the request, if available.
     /// </summary>
     public int? StatusCode { get; }
 
     /// <summary>
-    /// Request URL that failed
+    /// Gets the URL of the request that failed, if available.
     /// </summary>
     public string? RequestUrl { get; }
 
     /// <summary>
-    /// Request method (GET, POST, etc.)
+    /// Gets the HTTP method used by the request, if available.
     /// </summary>
     public string? Method { get; }
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="HttpRequestException"/> class.
+    /// </summary>
+    /// <param name="message">The message that describes the error.</param>
+    /// <param name="requestUrl">The URL of the request that failed, if available.</param>
+    /// <param name="method">The HTTP method used by the request, if available.</param>
+    /// <param name="statusCode">The HTTP status code returned by the request, if available.</param>
+    /// <param name="innerException">The exception that caused the current exception, if available.</param>
     public HttpRequestException(string message, string? requestUrl = null, string? method = null, int? statusCode = null, Exception? innerException = null)
         : base(message, "HTTP_REQUEST_FAILED", requestUrl)
     {
